310e927e92438370b9d058cf10761fb41fa6e080
杉山哲平 <sugiteppei@icloud.com>
2024-06-11 22:50:18 +0900

    Add files via upload

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="header.css">
+</head>
+
+<header>
+    <div class="logo">
+        <img src="cafe/img/logo.png">
+    </div>
+
+    <div class="hea-text">
+        <a href="#" id="scrollToStart">はじめに</a>
+        <a href="#" id="scrollToStart2">体験</a>
+        <a href="contact.php">お問い合わせ</a>
+    </div>
+
+    <div class="sign">
+        <a href="#" id="signInBtn">サインイン</a>
+    </div>
+
+</header>
+
+<script src="header.js"></script>
